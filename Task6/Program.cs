e6d83aa5de564b9a0c2c7fbaa41c372ec04709f3
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-10 09:48:14 +0300

    Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0);
+    Console.WriteLine("Делится без остатка");
+else
+    Console.WriteLine("Не делится без остатка");
