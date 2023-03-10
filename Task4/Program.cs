82782312fa75cb59a27ffbd52bd7180999d691d6
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-10 09:35:32 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-ое число: ");
+int k = int.Parse(Console.ReadLine()!);
+if (n > m || n > k ) 
+Console.WriteLine(n);
+else if (m > n || m > k);
+Console.WriteLine(m);
+else if (m > n || m > k)
+Console.WriteLine(k);
