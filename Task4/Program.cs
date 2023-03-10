142106cd9aeb58908d62996840bfc7ebd968d5f2
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-10 12:40:33 +0300

    Done Tasks

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -5,9 +5,9 @@ Console.Write("Введите 2-ое число: ");
 int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите 3-ое число: ");
 int k = int.Parse(Console.ReadLine()!);
-if (n > m || n > k ) 
-Console.WriteLine(n);
-else if (m > n || m > k);
-Console.WriteLine(m);
-else if (m > n || m > k)
-Console.WriteLine(k);
+if (n > m & n > k ) 
+Console.WriteLine($"{n}");
+else if (m > n & m > k)
+Console.WriteLine($"{m}");
+else if (k > n & k > m)
+Console.WriteLine($"{k}");
