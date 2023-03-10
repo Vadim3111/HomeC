1a090ec4431badc09b619f79f05b56120eaa2966
Vadim <frdfrffrrffdggtg@gmail.com>
2023-03-10 09:14:26 +0300

    Done Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int m = int.Parse(Console.ReadLine()!);
+if (m > n)
+    Console.WriteLine($"Max = {m}");
+else 
+    Console.WriteLine($"Max = {n}");
